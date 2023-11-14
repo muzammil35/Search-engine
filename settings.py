b6cf298ade9c0ac77e2a7b0f508d3fd50fdc42e1
muzammil35 <145228299+muzammil35@users.noreply.github.com>
2023-11-14 11:13:19 -0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY="AIzaSyBy3bQRkgCVrV5Mf1I1iygskCKkWTHhJfw"
+SEARCH_ID="62a7e76cf4c2a484a"
+COUNTRY="ca"
+
+SEARCH_URL= "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+RESULT_COUNT=20
