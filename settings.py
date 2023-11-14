b6913ae573118c966a205128c40afefb7f11ebaa
muzammil35 <145228299+muzammil35@users.noreply.github.com>
2023-11-14 11:16:19 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
-SEARCH_KEY="AIzaSyBy3bQRkgCVrV5Mf1I1iygskCKkWTHhJfw"
-SEARCH_ID="62a7e76cf4c2a484a"
+
 COUNTRY="ca"
 
 SEARCH_URL= "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
